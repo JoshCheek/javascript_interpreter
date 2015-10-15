c53f17d6c1b883b8adf19a426ed6e05247a0dbac
Josh Cheek <josh.cheek@gmail.com>
2015-10-14 18:16:23 -0600

    Refactor stderr and exit call back to bin, wrap RKelly's syntax error

--- lib/js_simulated_blocking.rb
+++ lib/js_simulated_blocking.rb
@@ -1,14 +1,15 @@
 require 'rkelly'
 
 class JsSimulatedBlocking
+  SyntaxError = Class.new SyntaxError
+
   def self.from_string(raw_js, stdout:)
     parser = RKelly::Parser.new
     ast    = parser.parse(raw_js)
-    ast || raise(RKelly::SyntaxError, "parser did not return an ast")
-    JsSimulatedBlocking.new(ast, stdout: $stdout)
+    raise RKelly::SyntaxError, "parser did not return an ast" unless ast
+    JsSimulatedBlocking.new ast, stdout: stdout
   rescue RKelly::SyntaxError => err
-    $stderr.puts "Syntax error (#{err.message})"
-    exit 1
+    raise JsSimulatedBlocking::SyntaxError, "Syntax error (#{err.message})"
   end
 
   def call
